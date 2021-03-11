0d9e15a6782b3a696c0f67707eeeb63a2dffe28d
Levi <lw03232@gmail.com>
2021-03-11 10:43:39 -0600

    Added framework for EnemyStinger logic

---
+++ Assets/Scripts/EnemyStinger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStinger : MonoBehaviour
+{
+    PolygonCollider2D stingerPoly;
+    public AudioSource zapSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stingerPoly = gameObject.GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision is CapsuleCollider2D && collision.IsTouching(stingerPoly))
+        {
+            /* FILL IN PLAYER HEALTH/LIVES LOGIC HERE
+             * 
+             */ 
+            zapSound.Play();
+        }
+    }
+}
